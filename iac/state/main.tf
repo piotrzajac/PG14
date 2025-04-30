3e6b5db7e4f3be9667b33173d623224d8ebdc957
Piotr Zajac <p.zajac@accenture.com>
2025-04-30 11:11:22 +0200

    Use updated endpoint

--- iac/state/main.tf
+++ iac/state/main.tf
@@ -9,6 +9,7 @@ terraform {
 
 provider "azurerm" {
   use_msi         = true
+  msi_endpoint    = "http://169.254.169.254/metadata/identity/oauth2/v2.0/token"
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
   client_id       = var.client_id
