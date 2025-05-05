7715213c8e3e59b006d7e79d6487fe1a92ccd695
Piotr Zajac <p.zajac@accenture.com>
2025-05-05 13:16:33 +0200

    Keep only required variables

--- iac/solution/variables.tf
+++ iac/solution/variables.tf
@@ -1,13 +1,3 @@
-variable "tenant_id" {
-  description = "The Azure tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "The Azure subscription ID"
-  type        = string
-}
-
 variable "environment" {
   description = "The environment for all resources"
   type        = string
