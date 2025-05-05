479e1c879bf8df6c21ab775ae1c3a0d4c6228489
Piotr Zajac <p.zajac@accenture.com>
2025-05-05 10:18:31 +0200

    Store resource group and storage account names as viriables in github and anign naming with secrets.

--- iac/state/github.tf
+++ iac/state/github.tf
@@ -6,13 +6,23 @@ data "github_user" "current" {
 # Store secrets related to the Azure storage for the Terraform state in GitHub Actions
 resource "github_actions_secret" "azure" {
   for_each = {
-    STATE_CLIENT_ID       = azurerm_user_assigned_identity.msi.client_id
-    STATE_TENANT_ID       = data.azurerm_client_config.current.tenant_id
-    STATE_SUBSCRIPTION_ID = data.azurerm_client_config.current.subscription_id
+    TF_STATE_AZURE_CLIENT_ID       = azurerm_user_assigned_identity.msi.client_id
+    TF_STATE_AZURE_TENANT_ID       = data.azurerm_client_config.current.tenant_id
+    TF_STATE_AZURE_SUBSCRIPTION_ID = data.azurerm_client_config.current.subscription_id
   }
   repository      = var.github_repository
   secret_name     = each.key
   plaintext_value = each.value
+}
+
+resource "github_actions_variable" "backend_azure" {
+  for_each = {
+    TF_STATE_AZURE_RESOURCE_GROUP_NAME  = azurerm_resource_group.tf_state.name
+    TF_STATE_AZURE_STORAGE_ACCOUNT_NAME = azurerm_storage_account.tf_state_storage.name
+  }
+  repository    = var.github_repository
+  variable_name = each.key
+  value         = each.value
 }
 
 # Create environments in the GitHub repository
